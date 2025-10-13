25351b4d6fbd99ff12e15255c9c229a1114ae050
kongyaoxian <1274071938@qq.com>
2025-10-13 16:07:36 +0800

    feat: 测试husky

---
+++ src/pages/Counter.tsx
@@ -0,0 +1,17 @@
+// src/components/Counter.tsx
+import { useState } from 'react';
+
+interface CounterProps {
+  initialValue?: number;
+}
+
+export const Counter = ({ initialValue = 0 }: CounterProps) => {
+  const [count, setCount] = useState(initialValue);
+  return (
+    <div className='counter'>
+      // 故意出错：用双引号
+    <p>"Count: {count}"</p>
+      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+    </div>
+  );
+};
